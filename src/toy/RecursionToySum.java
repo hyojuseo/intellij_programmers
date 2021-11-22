9f494468ee3b5f898db5749f1e959087ef3c0788
seohyoju <kinnylovemy@naver.com>
2021-11-22 16:32:38 +0900

    사용자지정 숫자부터 end까지의합

--- src/toy/RecursionToySum.java
+++ src/toy/RecursionToySum.java
@@ -1,17 +1,18 @@
 package toy;
 
+//end까지의 합
 public class RecursionToySum {
     static int sum = 0;
 
-    public static String re(int data){
+    public static String re(int data, int end){
         sum += data;
-        if(data == 1){
+        if(data == end){
             return Integer.toString(data);
         } else{
-            return (data + "+" + re(data-1));
+            return (data + "+" + re(data-1, end));
         }
     }
     public static void main(String[] args) {
-        System.out.println(re(10) + " = " +sum);
+        System.out.println(re(10,5) + " = " +sum);
     }
 }
